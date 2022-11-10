6bed1321bf7464b0f7251d0cf2f8291cd899f234
heyho-time <heyho9292@gmail.com>
2022-11-10 16:18:58 +0900

    add prettierrc / fix css

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,21 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function NavBar() {
   const router = useRouter();
 
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Image alt="logo" src="/vercel.svg" width={100} height={80} />
+
       <Link href="/">
-        <div>Home</div>
+        <div className={`${styles.text} ${router.pathname === '/' ? styles.active : ''}`}>Home</div>
       </Link>
 
       <Link href="/about">
-        <div>About</div>
+        <div className={`${styles.text} ${router.pathname === '/about' ? styles.active : ''}`}>About</div>
       </Link>
     </nav>
   );
